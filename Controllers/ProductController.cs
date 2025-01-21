a99dbaa46c7329aed730a29728f6f5986a8cae05
Trudy Wong <trudypwong@gmail.com>
2025-01-20 21:37:03 -0800

    Show product list from ProductController instead of previous ShopController. Add new tab to topbar

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -15,14 +15,9 @@ public class ProductController : Controller
         _productRepo = productRepo;
     }
 
-    // NOTE: 
-    // - ATM using Shop/Index for product list 
-    // - I'll switch to Product/Index later
-    // - (I think that follows previous hw better - ie. Employee, Bank, etc hw)
-    // 
-    // public IActionResult Index()
-    // {
-    //     IEnumerable<ProductVM> products = _productRepo.GetAllProducts();
-    //     return View(products);
-    // }
+    public IActionResult Index()
+    {
+        IEnumerable<ProductVM> products = _productRepo.GetAllProducts();
+        return View(products);
+    }
 }
