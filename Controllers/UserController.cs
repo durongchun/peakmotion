e3d2b21dd7a7ada1fddb2348c36de57def335494
durongchun <durongchun@hotmail.com>
2025-03-07 14:11:29 -0800

    remove cookie when logout

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,17 +12,21 @@ namespace peakmotion.Controllers
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<HomeController> _logger;
+        private readonly CookieRepo _cookieRepo;
 
-        public UserController(SignInManager<IdentityUser> signInManager, PmuserRepo pmuserRepo, UserManager<IdentityUser> userManager, ILogger<HomeController> logger)
+        public UserController(SignInManager<IdentityUser> signInManager, PmuserRepo pmuserRepo, UserManager<IdentityUser> userManager, ILogger<HomeController> logger, CookieRepo cookieRepo)
         {
             _pmuserRepo = pmuserRepo;
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
+            _cookieRepo = cookieRepo;
         }
 
         public async Task<IActionResult> Logout()
         {
+            _cookieRepo.RemoveCookie("cart");
+
             var identityUser = await _userManager.GetUserAsync(User);
             if (identityUser != null && identityUser.Email != null)
             {
@@ -42,6 +46,8 @@ namespace peakmotion.Controllers
             {
                 _logger.LogInformation("Could not find the Identity user.");
             }
+
+
 
             return Redirect("/Identity/Account/Login");
         }
