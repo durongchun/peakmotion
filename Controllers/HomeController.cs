d5f0115ecba438666dce1db61a11abf0e52726dc
Trudy Wong <trudypwong@gmail.com>
2025-03-08 13:13:57 -0800

    Link to custom access denied page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,4 +33,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public IActionResult AccessDenied()
+    {
+        return View();
+    }
 }
