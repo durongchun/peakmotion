50cf4746f787918118975c0cc56bb481a8a0e1a4
durongchun <durongchun@hotmail.com>
2025-03-09 09:06:58 -0700

    add selected color and size

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -129,6 +129,8 @@ public class ProductController : BaseController
         {
             return NotFound();
         }
+        var colors = _productRepo.GetProductAttributes(id, "color");
+        var sizes = _productRepo.GetProductAttributes(id, "size");
 
         // Map the database model to the view model
         var productDetailViewModel = new ProductDetailVM
@@ -139,7 +141,8 @@ public class ProductController : BaseController
             Unitprice = product.Regularprice,
             Qty = product.Qtyinstock,
             Fkcategoryid = 0,
-            // Fkcategory = null
+            Colors = colors,
+            Sizes = sizes,
         };
 
         var productVM = new ProductVM
